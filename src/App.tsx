45f54fdf6cbe421946dc9586286cf85d0c3ca463
Aresso93 <135212764+Aresso93@users.noreply.github.com>
2023-10-31 17:54:35 +0100

    /

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,28 @@ import './App.css'
 import products from '../store/products.json'
 import { ProductList } from './components/product-list';
 import { CartButton } from './components/cart-button';
+import IconButton from '@mui/material/IconButton';
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+
 
 console.log(products);
 
 const productsArray = products
 
 function App() {
-  
-  const [data,setData]=useState(
-    
-    []);
 
   return (
     <>
-      <CartButton/>
+     <IconButton 
+     color="primary" 
+     aria-label="see shopping cart"
+     onClick={() => 
+      console.log('AAAA')
+
+    }
+     >
+        <AddShoppingCartIcon />
+    </IconButton>
         
       <ProductList
       products={products}
